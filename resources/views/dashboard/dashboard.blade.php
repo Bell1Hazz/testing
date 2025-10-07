dc9f99b895a093af715269c5c642f029aff33acd
Dzaky Perwira Yasig <dzakyperwira1@gmail.com>
2025-10-07 12:44:41 +0700

    feat: add dashboard

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Jawa</title>
+    <title>Mantap!</title>
 </head>
 <body>
-    <h1>Halaman jawa</h1>
+    <h1>Halaman Mantap!</h1>
 </body>
 </html>
