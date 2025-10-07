924249a7184b9375f150430d223780281b3072d3
Dzaky Perwira Yasig <dzakyperwira1@gmail.com>
2025-10-07 12:50:19 +0700

    feat: add dashboard2

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Jawa</title>
+    <title>Bagus!</title>
 </head>
 <body>
-    <h1>Halaman jawa</h1>
+    <h1>Halaman Bagus!</h1>
 </body>
 </html>
