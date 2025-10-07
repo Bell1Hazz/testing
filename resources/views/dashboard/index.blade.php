ee590f63b48debbbd2882d68cfe41f6f9cd4d371
Fathir <fathirulfatih29@gmail.com>
2025-10-07 10:06:26 +0700

    feat: change content

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>testing</title>
+    <title>fathir</title>
 </head>
 <body>
-    <h1>Hello World!</h1>
+    <h1>Hello dzaku!</h1>
 </body>
 </html>
